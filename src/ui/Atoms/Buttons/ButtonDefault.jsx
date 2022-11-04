49f8c443e6c637b054842e5da7e017989b3d5705
mostafizur99 <coder.mr11@gmail.com>
2022-11-04 15:02:23 +0600

    test: buttonDefault tested

--- src/ui/Atoms/Buttons/ButtonDefault.jsx
+++ src/ui/Atoms/Buttons/ButtonDefault.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 
 function ButtonDefault(props) {
     const { variant = 'primary', children } = props
-    const backgroundColor = variant == 'primary' ? 'bg-blue-500 text-white' : variant == 'secondary' ? 'bg-slate-400' : variant == 'success' ? 'bg-green-500 text-white' : 'bg-rose-500 text-white'
+    const backgroundColor = variant === 'primary' ? 'bg-blue-500 text-white' : variant === 'secondary' ? 'bg-slate-400' : variant === 'success' ? 'bg-green-500 text-white' : 'bg-rose-500 text-white'
     return (
         <button className={`${backgroundColor} px-2 py-1 rounded`}>
-            Button
+            {children}
         </button>
     )
 }
