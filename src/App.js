efd7c6339f52f1949b6d2e7ddfea81cf2b3acca6
mostafizur99 <coder.mr11@gmail.com>
2022-11-07 21:39:47 +0600

    test: atom in section tested

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import ButtonDefault from '../src/ui/Atoms/Buttons/ButtonDefault';
 
 function App() {
   return (
-    <div>
-      <ButtonDefault variant={'success'}>Test Button</ButtonDefault>
+    <div className='my-5'>
+      <div className='container mx-auto py-10 bg-slate-100'>
+        <div className='text-center'>
+          <h4 className='font-bold'>Title Here</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati animi debitis consectetur veniam tempore aut quidem ea blanditiis, nesciunt autem nam laborum enim accusantium ratione ex eius hic, similique nihil quae corrupti fugit repellendus minima magni error. Corrupti deserunt, laudantium sed deleniti suscipit, necessitatibus fugiat maxime expedita quia nemo doloribus.</p>
+        </div>
+        <div className='flex justify-center mt-4'>
+          <ButtonDefault variant={'success'} size={'large'}>Button</ButtonDefault>
+        </div>
+      </div>
+
     </div>
   );
 }
